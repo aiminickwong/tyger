7b945e74ba86a6515a80d4f1659e24d1cc352aab
morph1904 <morph1904@gmail.com>
2018-04-08 12:54:16 +0100

    Fixed malformed nginx conf generation preventing HTTPS redirect.

--- tygerapp/nginx.py
+++ tygerapp/nginx.py
@@ -8,28 +8,40 @@ from tygerapp import shell
 @login_required
 def set_conf(request, proxy):
     config = nginx.Conf()
-    server = nginx.Server()
+    serverHTTP = nginx.Server()
+    serverHTTPS = nginx.Server()
     print(proxy.domain)
     print(proxy.ssl)
     print(proxy.letsencrypt)
     print(proxy.rewriteHTTPS)
     print(proxy.proxypass)
 
-    server.add(
+    serverHTTP.add(
         nginx.Key('listen', '*:80'),
         nginx.Key('server_name', proxy.domain),
 
     )
 
     if proxy.rewriteHTTPS:
-        server.add(
+        serverHTTP.add(
             nginx.Key('return', '301 https://$server_name$request_uri'),
 
+        )
+    else:
+        serverHTTP.add(
+            nginx.Location(
+                '/',
+                nginx.Key('proxy_pass', proxy.proxypass),
+                nginx.Key('proxy_set_header', 'Host $host'),
+                nginx.Key('proxy_set_header', 'X-Real-IP $remote_addr'),
+                nginx.Key('proxy_set_header', 'X-Forwarded-For $proxy_add_x_forwarded_for'),
+            )
         )
 
     if proxy.ssl:
         letsencrypt.generate_cert(request, proxy)
-        server.add(
+        serverHTTPS.add(
+            nginx.Key('server_name', proxy.domain),
             nginx.Key('listen', '*:443 ssl'),
             nginx.Key('ssl_protocols', 'TLSv1 TLSv1.1 TLSv1.2'),
             nginx.Key('ssl_certificate', '/etc/letsencrypt/live/'+proxy.domain+'/fullchain.pem'),
@@ -37,7 +49,7 @@ def set_conf(request, proxy):
 
         )
 
-    server.add(
+    serverHTTPS.add(
         nginx.Location(
             '/',
             nginx.Key('proxy_pass', proxy.proxypass),
@@ -46,7 +58,8 @@ def set_conf(request, proxy):
             nginx.Key('proxy_set_header', 'X-Forwarded-For $proxy_add_x_forwarded_for'),
         )
     )
-    config.add(server)
+    config.add(serverHTTP)
+    config.add(serverHTTPS)
     nginx.dumpf(config, '/etc/nginx/conf.d/' + proxy.domain + '.conf')
 
     shell.restart_nginx(request)
