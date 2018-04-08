0a8bf51b7b947f7ee327a2e2c063e32b98bdfc17
morph1904 <morph1904@gmail.com>
2018-04-08 12:11:45 +0100

    Added some shell functionality for letsencrypt

--- tygerapp/proxies/migrations/0001_initial.py
+++ tygerapp/proxies/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Site',
+            name='Proxy',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('domain', models.CharField(blank=True, max_length=255, verbose_name='Domain Name')),
--- tygerapp/proxies/migrations/0003_auto_20180402_1431.py
+++ tygerapp/proxies/migrations/0003_auto_20180402_1431.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('proxies', '0002_auto_20180402_1347'),
+        ('proxies', '0001_initial'),
     ]
 
     operations = [
