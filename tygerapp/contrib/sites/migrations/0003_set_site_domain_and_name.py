3a083816866fd35dc519b97c4ef1166bc5a8208b
morph1904 <morph1904@gmail.com>
2018-04-08 12:19:36 +0100

    Added some shell functionality for letsencrypt

--- tygerapp/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ tygerapp/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -9,7 +9,7 @@ from django.db import migrations
 
 def update_site_forward(apps, schema_editor):
     """Set site domain and name."""
-    Site = apps.get_model('proxies', 'Site')
+    Site = apps.get_model('sites', 'Site')
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
